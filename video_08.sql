14612ecfbb4843c2e8ef86415628d4c0c65fd143
jmcanovas <juanmacanovas@gmail.com>
2020-06-14 22:29:25 -0300

    add video_08

--- video_08.sql
+++ video_08.sql
@@ -10,3 +10,12 @@ SELECT * FROM productos;
 
 # delete row
 DELETE FROM productos WHERE id=20;
+
+# select all columns from table categorias
+SELECT * FROM categorias;
+
+# to empty and reset table productos
+SET FOREIGN_KEY_CHECKS=0;
+TRUNCATE TABLE productos;
+SET FOREIGN_KEY_CHECKS=1;
+
